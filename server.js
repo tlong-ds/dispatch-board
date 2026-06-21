d95b7ef8ff6ef779b494e3ae14cffbec4d7e0aac
Long <lytlong.pers@gmail.com>
2026-06-21 19:02:30 +0700

    fix: dino party player view not using the font

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors()); // Enable CORS for GitHub Pages frontend
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/dino-party', express.static('public'));
+app.use('/team', express.static('public'));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
